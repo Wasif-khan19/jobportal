282179d932d71f8b0f93647be3d48a42f4d7d072
Wasif <wk5599426@gmail.com>
2024-09-03 22:45:29 +0500

    edited

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -4,6 +4,11 @@ function Navbar() {
   return (
     <div className="bg-white">
         <div>
+          {/* left side */}
+            <div>
+              <h1>Job<span>Portal</span></h1>
+            </div>
+            {/* right side */}
             
         </div>
     </div>
